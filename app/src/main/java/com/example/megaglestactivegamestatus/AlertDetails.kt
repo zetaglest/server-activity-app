f50447fa9958aa1512da75dbf53f01371fe14a4c
andy5995 <andy400-dev@yahoo.com>
2022-02-02 23:33:26 -0600

    move notification code to separate class/file

---
+++ app/src/main/java/com/example/megaglestactivegamestatus/AlertDetails.kt
@@ -0,0 +1,76 @@
+package com.example.megaglestactivegamestatus
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.content.Context
+import android.content.Intent
+import android.content.res.Resources
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+// https://developer.android.com/training/notify-user/build-notification#kts
+class AlertDetails(private val mainThread: Context) {
+    private val channelID = "c42"
+    private val notificationID = 42
+
+    // show the notification
+    // getter for the call to with()
+    fun showNotification() {
+        with(NotificationManagerCompat.from(mainThread)) {
+            // notificationId is a unique int for each notification
+            // that you must define
+            notify(notificationID, builder.build())
+        }
+    }
+
+    // Create an explicit intent for an Activity in your app
+    private val intent = Intent(mainThread, AlertDetails::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+
+    // https://developer.android.com/guide/components/intents-filters#DeclareMutabilityPendingIntent
+    // Avoids the warning: "Missing PendingIntent mutability flag [UnspecifiedImmutableFlag]"
+    // https://developer.android.com/about/versions/12/behavior-changes-12#test-pending-intent
+    private fun getPendingFlag(): Int {
+        var pendingFlag = 0
+        if (Build.VERSION.SDK_INT >= 23) {
+            pendingFlag = FLAG_IMMUTABLE
+        }
+        return pendingFlag
+    }
+
+    private val pendingIntent: PendingIntent =
+        PendingIntent.getActivity(mainThread, 0, intent, getPendingFlag())
+
+    // Set the notification content
+    // https://developer.android.com/training/notify-user/build-notification#kts
+    private val builder = NotificationCompat.Builder(mainThread, channelID)
+        .setSmallIcon(R.drawable.notification_icon)
+        .setContentTitle("My MG notification")
+        .setContentText("Players Online")
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        // Set the intent that will fire when the user taps the notification
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true)
+
+    // For notifications, create a channel and set the importance
+    fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = Resources.getSystem().getString(R.string.channel_name)
+            val descriptionText = Resources.getSystem().getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                mainThread.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
--- app/src/main/java/com/example/megaglestactivegamestatus/MainActivity.kt
+++ app/src/main/java/com/example/megaglestactivegamestatus/MainActivity.kt
@@ -1,16 +1,8 @@
 package com.example.megaglestactivegamestatus
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
@@ -20,47 +12,23 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
-    private val cid = "c42"
-    private val nid = 42
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val alertDetails = AlertDetails(this)
         // Before you can deliver the notification on Android 8.0 and higher,
         // you must register your app's notification channel with the system by
         // passing an instance of NotificationChannel
         // to createNotificationChannel().
-        createNotificationChannel()
-
-        // Create an explicit intent for an Activity in your app
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-
-        // Set the notification content
-        // https://developer.android.com/training/notify-user/build-notification#kts
-        val builder = NotificationCompat.Builder(this, cid)
-            .setSmallIcon(R.drawable.notification_icon)
-            .setContentTitle("My MG notification")
-            .setContentText("Players Online")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            // Set the intent that will fire when the user taps the notification
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
+        alertDetails.createNotificationChannel()
 
        // val getPlayMG = GetPlayMG()
        // getPlayMG.getRaw(this)
         while (true) {
             val isWaiting = getActivity()
             if (isWaiting) {
-                // show the notification
-                with(NotificationManagerCompat.from(this)) {
-                    // notificationId is a unique int for each notification
-                    // that you must define
-                    notify(nid, builder.build())
-                }
+                alertDetails.showNotification()
             }
             runBlocking { delay(60000L) }
         }
@@ -102,23 +70,4 @@ class MainActivity : AppCompatActivity() {
         client.close()
         return isWaiting
     }
-
-    // For notifications, create a channel and set the importance
-    private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.channel_name)
-            val descriptionText = getString(R.string.channel_description)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(cid, name, importance).apply {
-                description = descriptionText
-            }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
 }
